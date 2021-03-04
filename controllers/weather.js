cd0a3d33aebf2b29ad67bf8705fc1ad068c5c3ba
matheus3006 <matheussouza18@live.com>
2021-03-04 13:35:17 -0300

    Retorno de erro caso a cidade procurada não exista

--- controllers/weather.js
+++ controllers/weather.js
@@ -7,40 +7,43 @@ exports.saveWeather = asyncHandler(async(req,res,next)=>{
     weather.setLang('pt_br');
     weather.setAPPID(process.env.API_KEY);
     const city = req.params.city;
-    // console.log(city);
     weather.setCity(city);
     try {
-        weather.getAllWeather(async(err, weatherData)=>{                 
-            if(!err){              
-                // Filtra os dados da API
-                const data = [{
+        weather.getAllWeather(async(err, weatherData)=>{
+            if(err){
+                console.log(err);
+                return res.status(404).json({success:false, msg:'Cidade não encontrada'});
+            }               
+                // Filtra os dados da API                 
+                const data = {
                     temperature: weatherData.main.temp,
                     humidity: weatherData.main.humidity,
                     weather: weatherData.weather[0].main,
                     description:weatherData.weather[0].description,
                     country: weatherData.sys.country,
                     name:weatherData.name
-                }];
-                
+                };
+                console.log('Valor de Data',data)
+                if(data !== null){                   
+             
                 // Salva os dados 
                 const weatherSaved = await insertWeather({
-                    name:data[0].name,
-                    country:data[0].country,
-                    temperature:data[0].temperature,
-                    humidity:data[0].humidity,
-                    weather:data[0].weather,
-                    description:data[0].description,               
+                    name:data.name,
+                    country:data.country,
+                    temperature:data.temperature,
+                    humidity:data.humidity,
+                    weather:data.weather,
+                    description:data.description,               
                 });
                 
-                res.status(200).json({success:true, data:data});
-            }else{
-                res.status(404);
-            }
-                
+                res.status(200).json({success:true, data:data});            
+                }           
+                           
         });
    
     } catch (err) {
-        console.error(err);
+        console.error('Erro:',err);
+        return res.status(404).json({msg:'Cidade não encontrada'})
     }
     
 });
